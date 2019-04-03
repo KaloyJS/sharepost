4dab740972c42e04d9cc8c35a9d2c5d0b222a7c0
Kaloy <kaloywebdev@gmail.com>
2019-04-02 22:19:27 -0400

    Added KaloyMVC framework and bootstrap.

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+	// DB params
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '123456');
+	define('DB_NAME', 'shareposts');
+
+	// App Root
+	define('APPROOT', dirname(dirname(__FILE__)));
+	// URL Root
+	define('URLROOT', 'http://localhost:100/sharepost');
+	// Site Name
+	define('SITENAME', 'SharePosts');
+	// App Version
+	define('APPVERSION', '1.0.0');
